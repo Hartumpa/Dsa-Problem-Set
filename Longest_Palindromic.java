f6650dacb624bce10fd2cb0372383676d58a943f
Hartumpa <iambeastonn@gmail.com>
2022-12-19 18:24:09 +0530

    first

---
+++ Longest_Palindromic.java
@@ -0,0 +1,10 @@
+public class Longest_Palindromic {
+    public static void main(String[] args) {
+        String s="Ramisgod";
+        StringBuilder str= new StringBuilder(s);
+        String s1;
+         s1= str.substring(1,3);
+
+        System.out.println(s1);
+    }
+}
